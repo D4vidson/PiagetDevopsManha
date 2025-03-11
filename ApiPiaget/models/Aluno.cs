d2faeea5bf25921b884483c156f9313438bf7806
D4vidson <davidsonsampaio08@gmail.com>
2025-03-11 09:40:55 -0300

    feat: projeto Devops

---
+++ ApiPiaget/models/Aluno.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiPiaget.models
+{
+    public class Aluno
+    {
+
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? RGM { get; set; }
+        public decimal Nota { get; set; }
+
+
+    }
+}
